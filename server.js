3deb43de508b73f88b42c58cc7edd3cb8ee740b5
Cristea Raul <148354161+karixwtf@users.noreply.github.com>
2026-01-12 22:37:59 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,11 +3,18 @@ const cors = require("cors");
 require("dotenv").config();
 const OpenAI = require("openai");
 
-const app = express();
+
+const emailRoutes = require("./emailroutes");
+
+const app = express();   // ✔️ trebuie să fie primul
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+
+app.use("/api", emailRoutes); 
+
 
 // Initialize OpenAI client
 const openai = new OpenAI({
