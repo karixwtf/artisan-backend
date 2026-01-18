fdbdfb193413f800061de822d50d7042b57c15a8
Cristea Raul <148354161+karixwtf@users.noreply.github.com>
2026-01-18 17:08:00 +0200

    Update emailroutes.js

--- emailroutes.js
+++ emailroutes.js
@@ -6,7 +6,7 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: process.env.SMTP_PORT,
-  secure: process.env.SMTP_SECURE === "true",
+  secure: process.env.SMTP_SECURE,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
